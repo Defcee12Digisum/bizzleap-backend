ae7eb0c4199581df619d7957631c8fcf85ee65c5
Defcee12Digisum <deftoinfiniti@hotmail.com>
2025-08-31 11:47:32 +0100

    Update database.js

--- config/database.js
+++ config/database.js
@@ -4,10 +4,10 @@ const path = require("path");
 
 // Database connection configuration
 const dbConfig = {
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: process.env.localhost:3306,
+  user: process.env.lontainc_def,
+  password: process.env.Goodness123@,
+  database: process.env.lontainc_bizzleap,
   port: process.env.DB_PORT || 3306,
   connectionLimit: 10,
   acquireTimeout: 60000,
