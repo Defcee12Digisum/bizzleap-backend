51722a01db22321f8ed4c0cb91d9557aa5068f8c
Defcee12Digisum <deftoinfiniti@hotmail.com>
2025-08-31 13:58:31 +0100

    Update database.js

--- config/database.js
+++ config/database.js
@@ -8,7 +8,7 @@ const dbConfig = {
   user: process.env.DB_USER,       // lontainc_def
   password: process.env.DB_PASSWORD, // Goodness123@
   database: process.env.DB_NAME,   // lontainc_bizzleap
-  port: process.env.DB_PORT || 3306
+  port: process.env.DB_PORT || 3306,
   connectionLimit: 10,
   acquireTimeout: 60000,
   timeout: 60000,
